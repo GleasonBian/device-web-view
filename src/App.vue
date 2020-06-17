7307a79873c607bd5cb5015b2af6316623fb50e4
Gleason <bianliuzhu@gmail.com>
2020-06-17 14:46:13 +0800

    引入图标库

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
+<!--
+ * @Description: 
+ * @Author: gleason
+ * @Github: https://github.com/GleasonBian
+ * @Date: 2020-06-17 10:49:05
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2020-06-17 14:45:14
+--> 
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@font-face {
+  font-family: "iconfont"; /* project id 1890392 */
+  src: url("//at.alicdn.com/t/font_1890392_ce36tjlumia.eot");
+  src: url("//at.alicdn.com/t/font_1890392_ce36tjlumia.eot?#iefix")
+      format("embedded-opentype"),
+    url("//at.alicdn.com/t/font_1890392_ce36tjlumia.woff2") format("woff2"),
+    url("//at.alicdn.com/t/font_1890392_ce36tjlumia.woff") format("woff"),
+    url("//at.alicdn.com/t/font_1890392_ce36tjlumia.ttf") format("truetype"),
+    url("//at.alicdn.com/t/font_1890392_ce36tjlumia.svg#iconfont") format("svg");
 }
-
-#nav {
-  padding: 30px;
+/* 菜单栏 图标 */
+.i-menu-one:before {
+  content: "\e611;";
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.i-menu-two:before {
+  content: "\e69d;";
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.i-menu-three:before {
+  content: "\e63c;";
+}
+.i-menu-four:before {
+  content: "\ea12;";
 }
 </style>
