eab6489759f070126be5c3a53fb3fbd7e71a241f
Gleason <bianliuzhu@gmail.com>
2020-06-17 11:27:24 +0800

    引入 mint UI,配置 mint

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
+/*
+ * @Description: 
+ * @Author: gleason
+ * @Github: https://github.com/GleasonBian
+ * @Date: 2020-06-17 10:49:05
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2020-06-17 11:18:28
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入 mint UI
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 Vue.config.productionTip = false
+
+// 使用 mint 
+Vue.use(MintUI)
 
 new Vue({
   router,
