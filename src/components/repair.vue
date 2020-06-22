f7c2b0627a6d58d15cd410fd4d445c6ccacc7735
liufeng <824242941@qq.com>
2020-06-22 14:54:55 +0800

    筛选

--- src/components/repair.vue
+++ src/components/repair.vue
@@ -1,33 +1,154 @@
 <!--
- * @Description: 维修计划
+ * @Description: 
  * @Author: gleason
  * @Github: https://github.com/GleasonBian
- * @Date: 2020-06-18 15:34:35
+ * @Date: 2020-06-18 15:21:21
  * @LastEditors: OBKoro1
- * @LastEditTime: 2020-06-19 14:29:57
+ * @LastEditTime: 2020-06-18 15:30:56
 -->
 <template>
-  <div>维修计划</div>
+  <div>
+    <van-dropdown-menu>
+      <van-dropdown-item
+        title="项目部"
+        v-model="form.proj_deptid"
+        :options="projDepts"
+        @change="getData('new')"
+      />
+      <van-dropdown-item
+        title="车辆名称"
+        v-model="form.equip"
+        :options="equipList"
+        @change="getData('new')"
+      />
+    </van-dropdown-menu>
+    <ul
+      class="wait-list"
+      v-infinite-scroll="getData"
+      infinite-scroll-disabled="moreLoading"
+      infinite-scroll-distance="0"
+      infinite-scroll-immediate-check="false"
+    >
+      <!--li数据遍历循环部分-->
+      <li
+        v-for="(item, index) in dataList"
+        :key="index"
+        @click="goDetails(index)"
+        class="wait-list-items"
+      >
+        <router-link to="{name:assignTasks}" class="list-item" tag="div">
+          <div class="title">
+            {{ item.equipname + " ( " + item.plant_no + " ) " }}
+            <span
+              class="status"
+            >{{ item.apply_state }}</span>
+          </div>
+          <div class="content">
+            <div>加油申请编号 : {{ item.code }}</div>
+            <div>项目部名称 : {{ item.project_name }}</div>
+            <div>计划维修时间 : {{ item.planstart }}</div>
+            <div>申请部门 : {{ item.deptnamesched }}</div>
+          </div>
+          <i class="enter iconfont i-entrance"></i>
+        </router-link>
+      </li>
+      <!--底部判断是加载图标还是提示“全部加载”-->
+      <li class="wait-list-loading">
+        <span v-show="moreLoading && !allLoaded">加载中...</span>
+        <span v-show="allLoaded">已全部加载</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { fixList, equiSelect, corpRank } from "@/getData";
 export default {
-  name: "repair",
+  name: "assign",
   data() {
-    return {};
+    return {
+      form: {
+        proj_deptid: "",
+        equip: ""
+      },
+      projDepts: [{ text: "全部", value: "" }],
+      equipList: [{ text: "全部", value: "" }],
+      dataList: [],
+      moreLoading: false,
+      allLoaded: false,
+      pageno: 1, // 代表取第0条后的数据-从0开始
+      pagesize: 10
+    };
   },
 
   computed: {},
 
-  methods: {},
+  methods: {
+    async getData(getNew) {
+      if (getNew) {
+        this.pagesize = 10;
+        this.pageno = 1;
+        this.dataList = [];
+      } else {
+        if (this.allLoaded) {
+          this.moreLoading = true;
+          return;
+        }
+      }
+      this.moreLoading = true;
+      let param = this.form;
+      param.pageno = this.pageno;
+      param.pagesize = this.pagesize;
+      const res = await fixList({ param: param });
+      if (res.status === 200) {
+        this.dataList.push(...res.data.list);
+        this.pagesize = res.data.pagesize;
+        if (res.data.pageno * this.pagesize < res.data.total) {
+          this.pageno++;
+          this.moreLoading = false;
+        } else {
+          this.allLoaded = true;
+          this.moreLoading = true;
+        }
+      }
+    },
+    async getEqList() {
+      const res = await equiSelect();
+      if (res.status === 200) {
+        res.data.map(item => {
+          item.text = item.name + " - " + item.plateno;
+          item.value = item.guid;
+        });
+        this.equipList.push(...res.data);
+      }
+    },
+    async getProjList() {
+      const res = await corpRank({ id: 3 });
+      if (res.status === 200) {
+        res.data.map(item => {
+          item.text = item.name;
+          item.value = item.guid;
+        });
+        this.projDepts.push(...res.data);
+      }
+    }
+  },
 
-  created() {},
+  created() {
+    this.$parent.more = true;
+    this.$parent.recept("oilApply");
+  },
 
-  mounted() {},
-
+  mounted() {
+    this.getData();
+    this.getEqList();
+    this.getProjList();
+  },
+  destroyed() {
+    this.$parent.more = false;
+  },
   components: {}
 };
 </script>
 
 <style scoped></style>
-
