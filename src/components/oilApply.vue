3560159be2af3610a33eadea663ada0b9a433513
liufeng <824242941@qq.com>
2020-06-22 09:45:54 +0800

    加油新建

--- src/components/oilApply.vue
+++ src/components/oilApply.vue
@@ -11,10 +11,11 @@
     <van-form @submit="onSubmit">
       <van-field
         readonly
+        required
         clickable
         name="datetimePicker"
         :value="form.start"
-        label="时间选择"
+        label="加油时间"
         placeholder="点击选择时间"
         @click="pickAddTime = true"
       />
@@ -22,7 +23,7 @@
         <van-datetime-picker
           v-model="currentDate"
           type="datetime"
-          title="选择完整时间"
+          title="选择加油时间"
           :min-date="minDate"
           :max-date="maxDate"
           @confirm="confirmTime1"
@@ -31,53 +32,62 @@
       </van-popup>
       <van-field
         readonly
+        required
         clickable
         name="picker"
-        :value="form.qeuip"
+        :value="qeuipname"
         label="车辆名称"
-        placeholder="点击选车辆"
+        placeholder="选择车辆"
+        :rules="[{ required: true, message: '请选择车辆' }]"
         @click="selEquip = true"
       />
       <van-popup v-model="selEquip" position="bottom">
-        <van-picker
-          show-toolbar
-          :columns="columns"
-          @confirm="choseEqp"
-          @cancel="selEquip = false"
-        />
+        <van-picker show-toolbar :columns="eqData" @confirm="choseEqp" @cancel="selEquip = false" />
       </van-popup>
       <van-field
-        v-model="password"
-        type="password"
-        name="密码"
-        label="密码"
-        placeholder="密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        readonly
+        required
+        clickable
+        name="picker"
+        :value="qeuipname"
+        label="项目名称"
+        placeholder="选择项目部"
+        :rules="[{ required: true, message: '请选择项目部' }]"
+        @click="selProj = true"
       />
+      <van-popup v-model="selProj" position="bottom">
+        <van-picker
+          show-toolbar
+          :columns="projData"
+          @confirm="choseProj"
+          @cancel="selProj = false"
+        />
+      </van-popup>
       <div style="margin: 16px;">
-        <van-button round block type="info" native-type="submit">
-          提交
-        </van-button>
+        <van-button round block type="info" native-type="submit">提交</van-button>
       </div>
     </van-form>
   </div>
 </template>
 
 <script>
+import { equiSelect } from "@/getData";
 export default {
   name: "repair",
   data() {
     return {
+      qeuipname: "",
       form: {
-        start: DateFormat(new Date()),
+        start: DateFormat(new Date())
       },
       pickAddTime: false,
       selEquip: false,
-      columns: ["杭州", "宁波", "温州", "嘉兴", "湖州"],
-      minDate: new Date(2020, 0, 1),
-      maxDate: new Date(2025, 10, 1),
+      selProj: false,
+      eqData: [],
+      minDate: new Date(1990, 0, 1),
+      maxDate: new Date(2090, 10, 1),
       currentDate: new Date(),
-      password: "",
+      password: ""
     };
   },
 
@@ -87,19 +97,40 @@ export default {
     onSubmit(values) {
       console.log("submit", values);
     },
-    choseEqp() {},
+    choseEqp(value, index) {
+      this.form.qeuip = value.guid;
+      this.qeuipname = value.text;
+      this.selEquip = false;
+    },
+    choseProj(value, index) {
+      this.form.qeuip = value.guid;
+      this.qeuipname = value.text;
+      this.selEquip = false;
+    },
+    async getEqList() {
+      const res = await equiSelect();
+      if (res.status === 200) {
+        this.eqData = res.data;
+        this.eqData.map(item => {
+          item.text = item.name + " - " + item.plateno;
+          item.value = item.guid;
+        });
+      }
+    },
     confirmTime1() {
       this.form.start = DateFormat(this.currentDate);
       console.log(this.currentDate);
       this.pickAddTime = false;
-    },
+    }
   },
 
   created() {},
 
-  mounted() {},
+  mounted() {
+    this.getEqList();
+  },
 
-  components: {},
+  components: {}
 };
 </script>
 
